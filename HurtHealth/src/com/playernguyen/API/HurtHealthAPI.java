cd66a2dcf99b5b1e5e7b2491081569fd667b07b6
PlayerNguyen <daudaua3782@gmail.com>
2015-05-01 19:26:28 +0700

    Next Gen 1.1 beta

---
+++ HurtHealth/src/com/playernguyen/API/HurtHealthAPI.java
@@ -0,0 +1,5 @@
+package com.playernguyen.API;
+
+public class HurtHealthAPI {
+
+}
---
+++ HurtHealth/src/com/playernguyen/SettingManger.java
@@ -0,0 +1,30 @@
+package com.playernguyen;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class SettingManger {
+
+	private File file = new File(HurtHealth.getHurtHealth().getDataFolder(), "config.yml");
+	
+	private FileConfiguration config;
+	
+	private SettingManger sm = new SettingManger();
+	
+	public SettingManger getSetting(){
+		return sm;
+	}
+	
+	public void setup(){
+		if(!file.exists()){
+			try {
+				config.save(file);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
